07906e110f9869fc8de2f72c8e776b04d5746d8f
Sadaatva <150649176+Sadaatva@users.noreply.github.com>
2023-11-24 19:55:12 +0400

    Update list.cpp
    Added constructors and operator

--- list/list.cpp
+++ list/list.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+
 template<typename T>
+
 class List {
-private:
+ private:
     struct Node {
         T element;
         Node* prev;
@@ -13,8 +15,29 @@ private:
     Node* last = nullptr;
     int elementsCount = 0;
 
-public:
+ public:
 
+    List(std::initializer_list<T> list_a = {}){
+    for (const auto& element : list_a){
+        PushBack(element);
+        }
+    }
+ 
+    List(const List& M){
+        for (Node* node = M.first; node != nullptr; node = node -> next){
+            PushBack(node -> element);
+        }
+    }
+    
+    List& operator= (const List& M){
+        if (this != &M) {
+            Clear();
+            for (Node* node = M.first; node != nullptr; node = node -> next) {
+                PushBack(node -> element);
+            }
+        }
+        return *this;
+    }
 
 };
 
